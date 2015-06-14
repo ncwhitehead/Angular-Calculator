a673f46604ff1b94d1f2ea8fcbd68053aa6f59c2
Nicole Whitehead <ncwhitehead@gmail.com>
2015-06-14 11:44:26 -0500

    Established the basic plating of the app and base expression functionality.

---
+++ app/app.js
@@ -0,0 +1,21 @@
+angular.module('calcApp', []).controller('CalcController',
+    function($scope) {
+        $scope.currentExpression = "";
+
+        $scope.buildExpression = function (operand) {
+          $scope.currentExpression += operand;
+        }
+
+        $scope.clearExpression = function () {
+          $scope.currentExpression = "";
+        }
+
+        $scope.evaluateExpression = function () {
+          $scope.currentExpression = eval($scope.currentExpression);
+        }
+
+        $scope.removeFromExpression = function () {
+          $scope.currentExpression = ($scope.currentExpression).substring(0, ($scope.currentExpression).length-1);
+        }
+    }
+);

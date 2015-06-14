8514b7c66f5a07435430a57a0c1f2da94e82c64c
Nicole Whitehead <ncwhitehead@gmail.com>
2015-06-14 13:17:51 -0500

    Adjusted the alignment within the app

--- app/app.js
+++ app/app.js
@@ -1,23 +1,23 @@
 angular.module('calcApp', []).controller('CalcController',
-    function($scope) {
-        $scope.currentExpression = "0";
-        $scope.lastExpression = "";
+  function($scope) {
+      $scope.currentExpression = "";
+      $scope.lastExpression = "";
 
-        $scope.buildExpression = function (operand) {
-          $scope.currentExpression += operand.toString();
-        }
+      $scope.buildExpression = function (operand) {
+        $scope.currentExpression += operand.toString();
+      }
 
-        $scope.clearExpression = function () {
-          $scope.currentExpression = "";
-        }
+      $scope.clearExpression = function () {
+        $scope.currentExpression = "";
+      }
 
-        $scope.evaluateExpression = function () {
-          $scope.lastExpression = $scope.currentExpression + "=";
-          $scope.currentExpression = eval($scope.currentExpression);
-        }
+      $scope.evaluateExpression = function () {
+        $scope.lastExpression = $scope.currentExpression + "=";
+        $scope.currentExpression = eval($scope.currentExpression);
+      }
 
-        $scope.removeFromExpression = function () {
-          $scope.currentExpression = ($scope.currentExpression).substring(0, ($scope.currentExpression).length-1);
-        }
-    }
+      $scope.removeFromExpression = function () {
+        $scope.currentExpression = ($scope.currentExpression).substring(0, ($scope.currentExpression).length-1);
+      }
+  }
 );
